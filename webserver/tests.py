8bd71e0c0a4bce540b5bac104fe75542e19be917
yiihannn <100120612+yiihannn@users.noreply.github.com>
2023-02-23 21:09:55 -0800

    Add some tests.

--- webserver/tests.py
+++ webserver/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from webserver.models import Profile
+
 
 # Create your tests here.
+class ProfileModelTests(TestCase):
+    def test_user_name(self):
+        user = User.objects.create_user(username="annyshi", first_name="Anny", last_name="Shi")
+        profile = Profile(user=user)
+        self.assertEqual("annyshi", profile.user.username)
+        self.assertEqual("Anny", profile.user.first_name)
+        self.assertEqual("Shi", profile.user.last_name)
